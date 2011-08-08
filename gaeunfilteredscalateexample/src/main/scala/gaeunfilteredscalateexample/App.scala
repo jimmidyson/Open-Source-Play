40c3188c331ee2bc5379ee558b7362618cb8a5e7
jimmidyson <jimmidyson@gmail.com>
2011-08-08 11:38:21 +0100

    * Added conversion from SNAPSHOT version to valid GAE version using gmaven plugin * Added in form parameter handling

--- gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/App.scala
+++ gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/App.scala
@@ -4,10 +4,12 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.scalate._
 
+import QParams._
+
 import org.fusesource.scalate.TemplateEngine
 import org.fusesource.scalate.layout.DefaultLayoutStrategy
 
-class App extends unfiltered.filter.Plan {
+class App extends unfiltered.filter.Plan with com.weiglewilczek.slf4s.Logging {
   val templateDirs = List(new java.io.File("WEB-INF/scalate"))
   val scalateMode = "production"
   implicit val engine = new TemplateEngine(templateDirs, scalateMode)
@@ -16,6 +18,16 @@ class App extends unfiltered.filter.Plan {
   engine.layoutStrategy = new DefaultLayoutStrategy(engine, "layouts/default.ssp")
 
   def intent = {
-    case req => Ok ~> Scalate(req, "templates/hello.ssp")
+    case req @ Path("/") => req match {
+      case GET(_) => Ok ~> Scalate(req, "templates/hello.ssp")
+      case POST(_) => {
+        val Params(form) = req
+        logger.info("Received post with name: " + form("name"))
+        form("name") match {
+          case Nil => Redirect("/")
+          case _ => Ok ~> Scalate(req, "templates/hello.ssp", ("name", form("name")(0)))
+        }
+      }
+    }
   }
 }
