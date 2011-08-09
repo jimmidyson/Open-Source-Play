4519c2854f076b55310aee62b8c0d148f188854b
jimmidyson <jimmidyson@gmail.com>
2011-08-09 10:07:43 +0100

    * Added a goodbye plan as a sample for multiple plans * Updated layotu to use HTML5 - just for fun to see some validation

--- gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/Main.scala
+++ gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/Main.scala
@@ -2,6 +2,6 @@ package gaeunfilteredscalateexample
 
 object Main extends com.weiglewilczek.slf4s.Logging {
   def main(args: Array[String]) {
-    val server = unfiltered.jetty.Http(8080).filter(new App()).run
+    val server = unfiltered.jetty.Http(8080).filter(new SayHelloPlan).filter(new SayGoodbyePlan).run
   }
 }
---
+++ gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/plans.scala
@@ -0,0 +1,57 @@
+package gaeunfilteredscalateexample
+
+import unfiltered.request._
+
+import unfiltered.response._
+import unfiltered.scalate._
+
+import QParams._
+
+import org.fusesource.scalate.TemplateEngine
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+
+abstract class ScalateConfigurator {
+  val templateDirs = List(new java.io.File("WEB-INF/scalate"))
+  val scalateMode = "production"
+  implicit val engine = new TemplateEngine(templateDirs, scalateMode)
+  engine.allowReload = false
+  engine.workingDirectory = new java.io.File("WEB-INF/scalate")
+  engine.layoutStrategy = new DefaultLayoutStrategy(engine, "layouts/default.ssp")
+}
+
+class SayHelloPlan extends ScalateConfigurator with unfiltered.filter.Plan with com.weiglewilczek.slf4s.Logging {
+  def intent = {
+    case req @ GET(Path(Seg(Nil))) => Ok ~> Scalate(req, "templates/hello.ssp")
+    case req @ Path(Seg("hello" :: Nil)) => req match {
+      case GET(_) => Ok ~> Scalate(req, "templates/hello.ssp")
+      case POST(_) => {
+        val Params(form) = req
+            logger.info("Received post with name: " + form("name"))
+            form("name") match {
+              case Nil => Redirect("/hello")
+              case _ => Ok ~> Scalate(req, "templates/hello.ssp", ("name", form("name")(0)))
+        }
+      }
+    }
+    case req @ GET(Path(Seg("hello" :: name :: Nil))) => Ok ~> Scalate(req, "templates/hello.ssp", ("name", name))
+    case _ => Pass
+  }
+}
+
+class SayGoodbyePlan extends ScalateConfigurator with unfiltered.filter.Plan with com.weiglewilczek.slf4s.Logging {
+  def intent = {
+    case req @ Path(Seg("goodbye" :: Nil)) => req match {
+      case GET(_) => Ok ~> Scalate(req, "templates/goodbye.ssp")
+      case POST(_) => {
+        val Params(form) = req
+            logger.info("Received post with name: " + form("name"))
+            form("name") match {
+              case Nil => Redirect("/goodbye")
+              case _ => Ok ~> Scalate(req, "templates/goodbye.ssp", ("name", form("name")(0)))
+        }
+      }
+    }
+    case req @ GET(Path(Seg("goodbye" :: name :: Nil))) => Ok ~> Scalate(req, "templates/goodbye.ssp", ("name", name))
+    case _ => Pass
+  }
+}
