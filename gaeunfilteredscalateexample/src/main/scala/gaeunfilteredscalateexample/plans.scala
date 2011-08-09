73ae587e42d035bb552be216ea28b7272de9dcb0
jimmidyson <jimmidyson@gmail.com>
2011-08-09 12:30:43 +0100

    * Example of mixing up templating technology - used Jade for most HTML output now

--- gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/plans.scala
+++ gaeunfilteredscalateexample/src/main/scala/gaeunfilteredscalateexample/plans.scala
@@ -16,42 +16,46 @@ abstract class ScalateConfigurator {
   implicit val engine = new TemplateEngine(templateDirs, scalateMode)
   engine.allowReload = false
   engine.workingDirectory = new java.io.File("WEB-INF/scalate")
-  engine.layoutStrategy = new DefaultLayoutStrategy(engine, "layouts/default.ssp")
+  engine.layoutStrategy = new DefaultLayoutStrategy(engine, "layouts/default.jade")
 }
 
 class SayHelloPlan extends ScalateConfigurator with unfiltered.filter.Plan with com.weiglewilczek.slf4s.Logging {
+  val template = "templates/hello.ssp"
+
   def intent = {
     case req @ GET(Path(Seg(Nil))) => Ok ~> Scalate(req, "templates/hello.ssp")
     case req @ Path(Seg("hello" :: Nil)) => req match {
-      case GET(_) => Ok ~> Scalate(req, "templates/hello.ssp")
+      case GET(_) => Ok ~> Scalate(req, template)
       case POST(_) => {
         val Params(form) = req
             logger.info("Received post with name: " + form("name"))
             form("name") match {
               case Nil => Redirect("/hello")
-              case _ => Ok ~> Scalate(req, "templates/hello.ssp", ("name", form("name")(0)))
+              case _ => Ok ~> Scalate(req, template, ("name", form("name")(0)))
         }
       }
     }
-    case req @ GET(Path(Seg("hello" :: name :: Nil))) => Ok ~> Scalate(req, "templates/hello.ssp", ("name", name))
+    case req @ GET(Path(Seg("hello" :: name :: Nil))) => Ok ~> Scalate(req, template, ("name", name))
     case _ => Pass
   }
 }
 
 class SayGoodbyePlan extends ScalateConfigurator with unfiltered.filter.Plan with com.weiglewilczek.slf4s.Logging {
+  val template = "templates/goodbye.jade"
+
   def intent = {
     case req @ Path(Seg("goodbye" :: Nil)) => req match {
-      case GET(_) => Ok ~> Scalate(req, "templates/goodbye.ssp")
+      case GET(_) => Ok ~> Scalate(req, template)
       case POST(_) => {
         val Params(form) = req
             logger.info("Received post with name: " + form("name"))
             form("name") match {
               case Nil => Redirect("/goodbye")
-              case _ => Ok ~> Scalate(req, "templates/goodbye.ssp", ("name", form("name")(0)))
+              case _ => Ok ~> Scalate(req, template, ("name", form("name")(0)))
         }
       }
     }
-    case req @ GET(Path(Seg("goodbye" :: name :: Nil))) => Ok ~> Scalate(req, "templates/goodbye.ssp", ("name", name))
+    case req @ GET(Path(Seg("goodbye" :: name :: Nil))) => Ok ~> Scalate(req, template, ("name", name))
     case _ => Pass
   }
 }
